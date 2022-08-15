832f3d94b33205357c7318c376ed5191a01e1249
michaelgamston <michaelgamston1@gmail.com>
2022-08-15 11:58:18 +0100

    adding more orginal files

--- ESP32-CAM/src/main.cpp
+++ ESP32-CAM/src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
+#include "camera_funcs.h"
+#include "serial_comms.h"
 
-void setup() {
-  // put your setup code here, to run once:
+// timing variables
+unsigned long start;
+unsigned long end;
+const uint32_t IMAGE_SIZE = H * W;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  setup_spi();
+  setup_camera(FRAME_SIZE);
+  // Neural network object
+  Serial.println("setup");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // print_quant_conversions();
+  start = millis();
+  // copy previous frame for background subtraction
+  update_frame();
+  // take pic
+  capture_still();
+  if (motion_detect())
+  {
+    Serial.println("Motion detected");
+  }
+  send_image();
+  end = millis();
+  Serial.printf("Took %d ms\n", end-start);
+  delay(2000);
 }
